a893041eec234063b59abe49d74118b2d2824071
이정철 <ghop0822@gmail.com>
2025-10-29 17:53:37 +0900

    Add files via upload

---
+++ ch05/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("숫자를 입력하세요: ");
+
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+
+            if(number % 2 == 0)
+            {
+                Console.WriteLine($"{number}는 짝수입니다.");
+            }
+            else if(number == 0)
+            {
+                Console.WriteLine("0은 짝수도 홀수도 아닙니다.");
+            }
+            else
+            {
+                Console.WriteLine($"{number}는 홀수입니다.");
+            }
+        }
+    }
+}
